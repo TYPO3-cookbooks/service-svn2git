648a93efbe1e4ec4f73a1e5cbbacc95ca1e47c5a
Steffen Gebert <steffen.gebert@typo3.org>
2016-01-28 06:51:25 +0100

    Bump version to 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "fabien.udriot@typo3.org"
 license          "Apache 2.0"
 description      "Recipe for synchronizing TYPO3 SVN extension to Github"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
